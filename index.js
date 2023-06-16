e712eeb6d63ea6a8646093d4a3fadd1fbdee4c6d
mukoyadarius <mukoya.darius@student.moringaschool.com>
2023-06-16 12:57:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+let employee={
+    name:"Mercy",
+    streetAdress:"Nairobi"
+};
+function updateEmployeeWithKeyAndValue(object,key,value){
+    let newObj={...object}
+    newObj[key]=value;
+    return newObj;
+
+}
+updateEmployeeWithKeyAndValue(employee,"sam","11 Broadway");
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object,key,value){
+    object[key]=value;
+    return object;
+
+}
+function deleteFromEmployeeByKey(object,key){
+    let newObj={...object}
+    delete newObj[key]
+    return newObj;
+}
+deleteFromEmployeeByKey(employee,streetAdress);
+
+function destructivelyDeleteFromEmployeeByKey(object,key){
+    delete object[key];
+    return object;
+}
